e9eba9aae5e935044032dc15fb1bde788464eaac
tastas-shimizu <shimizu@tas-tas.jp>
2025-06-27 21:40:07 +0900

    Game https://ja.react.dev/learn/tutorial-tic-tac-toe

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import Counter from './Counter'
+import Game from './Game'
 
 function App() {
     return (
         <main style={{ textAlign: 'center' }}>
             <h1>Hello Vite + React!</h1>
-            <Counter/>
+            <Game/>
         </main>
     )
 }
