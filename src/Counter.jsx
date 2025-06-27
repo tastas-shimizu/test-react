7fc15d4315a4c4ca149deb7218216a824bd5d0e9
tastas-shimizu <shimizu@tas-tas.jp>
2025-06-27 21:27:50 +0900

    minimal vite + react

---
+++ src/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function Counter() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <p>You clicked {count} times.</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+        </>
+    )
+}
+
+export default Counter
